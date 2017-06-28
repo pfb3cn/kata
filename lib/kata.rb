85e81c85b1fe97cd38cb59f198293bac1aa502bf
Phil <phil.boothe@gmail.com>
2017-06-28 10:56:06 -0400

    Add test cases for some numbers

--- lib/kata.rb
+++ lib/kata.rb
@@ -2,10 +2,31 @@ require "kata/version"
 
 module Kata
   class RomanNumerals
-
     def self.convert(number)
-      
-    end
+      first = ''
+      if (number / 10).truncate == 4
+        first = 'XL'
+      elsif (number/10).truncate == 10
+        first = 'C'
+      elsif (number/10).truncate == 100
+        first = 'M'
+      end
 
+      last = ''
+      last = case number % 10
+             when 1
+               'I'
+             when 2
+               'II'
+             when 4
+               'IV'
+             when 5
+               'V'
+             else
+               ''
+             end
+
+      first + last
+    end
   end
 end
--- test/test_kata.rb
+++ test/test_kata.rb
@@ -1,11 +1,33 @@
 require 'minitest_helper'
 
 class Kata::RomanNumerals
-
   describe '#convert' do
-    it 'converts 1 to the I' do
+    it 'converts 1 to I' do
       assert_equal 'I', Kata::RomanNumerals.convert(1)
     end
-  end
 
+    it 'converts 2 to II' do
+      assert_equal 'II', Kata::RomanNumerals.convert(2)
+    end
+
+    it 'converts 5 to V' do
+      assert_equal 'V', Kata::RomanNumerals.convert(5)
+    end
+
+    it 'converts 44 to XLIV' do
+      assert_equal 'XLIV', Kata::RomanNumerals.convert(44)
+    end
+
+    it 'converts 45 to XLV' do
+      assert_equal 'XLV', Kata::RomanNumerals.convert(45)
+    end
+
+    it 'converts 100 to C' do
+      assert_equal 'C', Kata::RomanNumerals.convert(100)
+    end
+
+    it 'converts 1002 to MII' do
+      assert_equal 'MII', Kata::RomanNumerals.convert(1002)
+    end
+  end
 end
